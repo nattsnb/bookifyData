a5231ac8384642dc8836fab95b40edc5425223e6
Natasza ASzatan <natiye.nb@gmai.com>
2024-11-24 14:57:19 +0100

    added number of revievs, deleted albums object, each venue has its own, changed venueAmenites to ones from project, added occassions to build filter later#

--- main.js
+++ main.js
@@ -3,32 +3,142 @@ import { saveAs } from "file-saver";
 import { addresses } from "/addresses.js";
 
 const venuesAmenities = [
-  { id: 1, name: "Wi-Fi" },
-  { id: 2, name: "Parking" },
-  { id: 3, name: "Stage" },
-  { id: 4, name: "Catering services" },
-  { id: 5, name: "Audio-visual equipment" },
-  { id: 6, name: "Outdoor space" },
-  { id: 7, name: "Dance floor" },
-  { id: 8, name: "Bridal suite" },
-  { id: 9, name: "Private dining rooms" },
-  { id: 10, name: "Bar service" },
-  { id: 11, name: "Breakout rooms" },
-  { id: 12, name: "Seating arrangements" },
-  { id: 13, name: "Decor services" },
-  { id: 14, name: "Projector and screen" },
-  { id: 15, name: "Sound system" },
-  { id: 16, name: "Lighting system" },
-  { id: 17, name: "Climate control" },
-  { id: 18, name: "Security staff" },
-  { id: 19, name: "Restrooms" },
-  { id: 20, name: "Photography area" },
-  { id: 21, name: "Kid-friendly play area" },
-  { id: 22, name: "Accessible facilities" },
-  { id: 23, name: "Green room" },
-  { id: 24, name: "Ticketing services" },
-  { id: 25, name: "Event planner services" }
+  {
+    amenities: [
+      { id: 0, name: "parking" },
+      { id: 1, name: "WiFi" },
+      { id: 2, name: "restaurant" },
+      { id: 3, name: "bar" },
+      { id: 4, name: "pool" },
+      { id: 5, name: "jacuzzi" },
+      { id: 6, name: "garden" },
+      { id: 7, name: "fitness centre" },
+      { id: 8, name: "playground" },
+      { id: 9, name: "24h reception" },
+      { id: 10, name: "speakers" },
+      { id: 11, name: "outdoor music" },
+      { id: 12, name: "indoor music" },
+      { id: 13, name: "karaoke" },
+      { id: 14, name: "library" },
+      { id: 15, name: "pet friendly" },
+    ],
+    rooomAmenities: [
+      { id: 0, name: "kitchen facilities" },
+      { id: 1, name: "bathroom facilities" },
+      { id: 2, name: "hypoallergenic bedding" },
+      { id: 3, name: "air conditioning" },
+      { id: 4, name: "safe" },
+      { id: 5, name: "tv" },
+    ],
+    handicapAccessibility: [
+      { id: 0, name: "wheelchair friendly" },
+      { id: 1, name: "blind friendly" },
+      { id: 2, name: "deaf friendly" },
+      { id: 3, name: "short-grown friendly" },
+    ],
+    neighbourhoods: [
+      { id: 0, name: "lake" },
+      { id: 1, name: "forest" },
+      { id: 2, name: "mountains" },
+      { id: 3, name: "sea" },
+      { id: 4, name: "national park" },
+      { id: 5, name: "river" },
+      { id: 6, name: "park" },
+      { id: 7, name: "mail" },
+      { id: 8, name: "zoo" },
+      { id: 9, name: "church" },
+      { id: 10, name: "old town" },
+      { id: 11, name: "historical monuments" },
+      { id: 12, name: "museum" },
+      { id: 13, name: "theatre" },
+      { id: 14, name: "cinema" },
+      { id: 15, name: "amusement park" },
+      { id: 16, name: "restaurant" },
+    ],
+    occasions: {
+      conference: {
+        requiredAmenities: [0, 1, 10, 12],
+        requiredRoomAmenities: [3, 5],
+        accessibility: [0, 2],
+        neighborhoods: [7, 11],
+      },
+      romanticGetaway: {
+        requiredAmenities: [2, 3, 5, 4],
+        requiredRoomAmenities: [2, 3],
+        accessibility: [],
+        neighborhoods: [3, 10, 11],
+      },
+      familyVacation: {
+        requiredAmenities: [8, 4, 6, 15],
+        requiredRoomAmenities: [1, 5],
+        accessibility: [0, 3],
+        neighborhoods: [1, 5, 15],
+      },
+      businessTrip: {
+        requiredAmenities: [0, 1, 9, 3],
+        requiredRoomAmenities: [3, 4],
+        accessibility: [0, 2],
+        neighborhoods: [7, 10],
+      },
+      wedding: {
+        requiredAmenities: [0, 6, 4, 11],
+        requiredRoomAmenities: [],
+        accessibility: [0],
+        neighborhoods: [1, 4],
+      },
+      wellnessRetreat: {
+        requiredAmenities: [4, 5, 7, 6],
+        requiredRoomAmenities: [0, 3],
+        accessibility: [0],
+        neighborhoods: [1, 2, 4],
+      },
+      musicFestival: {
+        requiredAmenities: [11, 12, 13],
+        requiredRoomAmenities: [],
+        accessibility: [0, 2],
+        neighborhoods: [1, 4],
+      },
+      culturalTrip: {
+        requiredAmenities: [14, 12],
+        requiredRoomAmenities: [],
+        accessibility: [1],
+        neighborhoods: [9, 11, 12, 13],
+      },
+      adventure: {
+        requiredAmenities: [],
+        requiredRoomAmenities: [1],
+        accessibility: [],
+        neighborhoods: [1, 2, 4],
+      },
+      beachHoliday: {
+        requiredAmenities: [4, 3, 2],
+        requiredRoomAmenities: [3, 5],
+        accessibility: [0],
+        neighborhoods: [3],
+      },
+      shoppingTrip: {
+        requiredAmenities: [0, 1],
+        requiredRoomAmenities: [],
+        accessibility: [],
+        neighborhoods: [7],
+      },
+      educationalTrip: {
+        requiredAmenities: [14],
+        requiredRoomAmenities: [],
+        accessibility: [],
+        neighborhoods: [12, 13],
+      },
+      party: {
+        requiredAmenities: [3, 11, 12, 13],
+        requiredRoomAmenities: [],
+        accessibility: [0],
+        neighborhoods: [6, 15],
+      },
+    },
+  },
 ];
+
+
 
 async function getPictureAddress() {
   const getPicsumResponse = await fetch("https://picsum.photos/282/186/");
@@ -53,13 +163,10 @@ function produceFeaturesArray(numberOfFeatures) {
 async function produceFakerData() {
   const venues = [];
   const venuesDetails = [];
-  const albums = [];
-  const data = { venues, venuesDetails, albums, venuesAmenities };
+  const data = { venues, venuesDetails, venuesAmenities };
 
   for (let i = 0; i < 100; i++) {
     const address = addresses[i];
-    const album = await produceAlbum();
-    albums.push(album);
 
     const venue = {
       id: i,
@@ -71,10 +178,10 @@ async function produceFakerData() {
       },
       pricePerNightInEURCent: faker.number.int({ max: 10000 }).toString(),
       rating: faker.number.float({ max: 5, fractionDigits: 1 }),
+      reviews: faker.number.float({ max: 0, fractionDigits: 156 }),
       capacity: faker.number.int({ max: 10 }),
       name: `${faker.word.adjective()} ${faker.word.noun()}`,
-      albumId: i,
-      coverPhoto: album[0]
+      images: await produceAlbum()
     };
     venues.push(venue);
 
